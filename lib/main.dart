778da40bf4afbebd178ae3274049ab011b46e70e
Harisivams <harisivam203@gmail.com>
2024-06-16 21:17:49 +0530

    practice files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 
@@ -13,11 +13,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',  
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("Exampssle"),),
-
+        appBar: AppBar(
+          title: Text("Example"),
+        ),
+        body: Container(
+          color: Colors.deepPurple,
+          margin: EdgeInsets.all(12),
+          child: Center(
+            child: Text(
+              "Container",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                
+              ),
+            ),
+          ),
+          height: 250,
+          width: 250,
+        ),
       ),
     );
   }
